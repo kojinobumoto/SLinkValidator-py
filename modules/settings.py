3d0e69feed0839443ec7a867d21cda03a4e53b50
kojinobumoto <koji.nobumoto@gmail.com>
2021-05-24 16:16:07 +0900

    let the default file format to be no BOM.
    let the default file format to be no BOM.

--- modules/settings.py
+++ modules/settings.py
@@ -4,5 +4,5 @@ def init():
                 , FILE_ENCODING
         PATH_TO_CHROME_DRIVER    = 'C:\Program Files (x86)\chromedriver\chromedriver.exe'
         NUM_IMPLICITLY_WAIT_SEC  = 10
-        FILE_ENCODING            = 'utf-8_sig'
+        FILE_ENCODING            = 'utf-8'
     
