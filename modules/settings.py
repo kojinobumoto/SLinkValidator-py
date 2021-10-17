8e5297ddb9f33ee68eaf40e7352527ffa217ce23
kojinobumoto <koji.nobumoto@gmail.com>
2021-10-17 19:22:05 +0900

    arameterized verify ssl, wait after driver.get()
    - parameterized verify ssl (boolean).
    - wait after driver.get() to avoid StaleElementReferenceException.

--- modules/settings.py
+++ modules/settings.py
@@ -2,9 +2,11 @@ def init():
         global PATH_TO_CHROME_DRIVER \
                 , NUM_IMPLICITLY_WAIT_SEC \
                 , FILE_ENCODING \
-                , INT_MAX_ATTEMMPTS_FOR_STALEELEMENT
-        PATH_TO_CHROME_DRIVER    = 'C:\Program Files (x86)\chromedriver\chromedriver.exe'
-        NUM_IMPLICITLY_WAIT_SEC  = 10
-        FILE_ENCODING            = 'utf-8'
-        INT_MAX_ATTEMMPTS_FOR_STALEELEMENT = 10
+                , INT_MAX_ATTEMMPTS_FOR_STALEELEMENT \
+                , INT_WAIT_SEC_AFTER_DRIVER_GET
+        PATH_TO_CHROME_DRIVER                 = 'C:\Program Files (x86)\chromedriver\chromedriver.exe'
+        NUM_IMPLICITLY_WAIT_SEC               = 30
+        FILE_ENCODING                         = 'utf-8'
+        INT_MAX_ATTEMMPTS_FOR_STALEELEMENT    = 10
+        INT_WAIT_SEC_AFTER_DRIVER_GET         = 2
     
